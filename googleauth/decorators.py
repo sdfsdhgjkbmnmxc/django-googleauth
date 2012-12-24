417568af94a82ef9b0e04a0be3335e744de79b7a
Maxim Oransky <om@ostrovok.ru>
2012-12-25 01:59:40 +0400

    debug removed

--- googleauth/decorators.py
+++ googleauth/decorators.py
@@ -10,7 +10,7 @@ from views import login
 def required(func):
     def decorated(request, *args, **kwargs):
         user = _get_user_or_none(request)
-        print func, 'user', user
+        #print func, 'user', user
         if not user:
             return HttpResponseRedirect(reverse(login))
         setattr(request, REQUEST_ATTR, user)
@@ -29,7 +29,7 @@ def if_logged_in_then_redirect_to_start_page(func):
 
 def _get_user_or_none(request):
     user_id = request.session.get(USER_SESSION_ID)
-    print request.path, 'user_id', user_id
+    #print request.path, 'user_id', user_id
     Model = get_model()
     try:
         return Model.objects.get(id=user_id)
